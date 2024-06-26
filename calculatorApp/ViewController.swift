96c1f23cbd75c131d3242c116b1be0f95f73c4f3
jihyeon <dkswlgus0314@naver.com>
2024-06-26 17:56:14 +0900

    level1_complete

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -10,10 +10,31 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    var numLable = UILabel()
+    var viewNum = 12345
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
     }
-
-
+    
+    func configureUI(){
+        //속성
+        view.backgroundColor = .black
+        numLable.backgroundColor = .black
+        numLable.textColor = .white
+        numLable.text = "\(viewNum)"
+        numLable.textAlignment = .right
+        numLable.font = .boldSystemFont(ofSize: 60)
+        
+        [numLable].forEach{view.addSubview($0)}
+        
+        //AutoLayout
+        numLable.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.top.equalToSuperview().offset(200)
+        }
+    }
 }
 
