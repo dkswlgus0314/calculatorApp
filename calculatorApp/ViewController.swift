a5770736c873dee92182206e175581b077a042c5
jihyeon <dkswlgus0314@naver.com>
2024-06-26 16:53:03 +0900

    Initial Commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by ahnzihyeon on 6/26/24.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    var numLable = UIView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
